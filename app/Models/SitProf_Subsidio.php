3afa55b47e155cbe1613e6207fd70c56d4273100
Yevheniy <yevheniykushnirenko@gmail.com>
2023-01-23 09:48:46 +0000

    project boilerplate with classes and migrations

---
+++ app/Models/SitProf_Subsidio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SitProf_Subsidio extends Model
+{
+    use HasFactory;
+}
